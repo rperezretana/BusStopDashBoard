3af74a78caf1c06373b4d056ccf5f4334d7e9a7f
Rolando Retana <rperezretana@hotmail.com>
2019-07-31 01:38:22 -0700

    Adding aditional comments

--- BusScheduleManager/Queries/BusRouteQuery.cs
+++ BusScheduleManager/Queries/BusRouteQuery.cs
@@ -9,17 +9,18 @@ using System.Threading.Tasks;
 
 namespace BusScheduleManager.Queries
 {
+    /// <summary>
+    /// This class takes care of the queries, mutations etc.
+    /// </summary>
     public class BusRouteQuery : ObjectGraphType
     {
         public BusRouteQuery(IBusRouteRepository busRouteRepository)
         {
             Field<ListGraphType<BusRouteType>>("BusStops",
-                arguments: new QueryArguments(new QueryArgument<IntGraphType> { Name = "hour" }, new QueryArgument<IntGraphType> { Name = "minute" }/*, new QueryArgument<IntGraphType> { Name = "stopId" }*/),
+                arguments: new QueryArguments(new QueryArgument<IntGraphType> { Name = "hour" }, new QueryArgument<IntGraphType> { Name = "minute" }),
                 resolve: context => {
                         var hour = context.GetArgument<int>("hour");
                         var minute = context.GetArgument<int>("minute");
-                        //var stopId = context.GetArgument<int>("stopId");
-                        // return busRouteRepository.GetRoutsForStop(hour, minute, stopId); this could be used in a different implementation to get an specific ID stop, but for the example we skip it.
                         return busRouteRepository.GetRoutsForAllTheStops(hour, minute);
                     });
         }
--- BusSchedulemanager.DataAccess/Repositories/Contracts/IBusRouteRepository.cs
+++ BusSchedulemanager.DataAccess/Repositories/Contracts/IBusRouteRepository.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 
 namespace BusSchedulemanager.DataAccess.Repositories.Contracts
 {
+    /// <summary>
+    /// Interfaces to use as connections to use dependency injection on the API.
+    /// This facilitate implementing unit test.
+    /// </summary>
     public interface IBusRouteRepository
     {
         IEnumerable<BusRoute> GetAll();
